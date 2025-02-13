1c69885916c8b099c32855ad89c504aea3c088a1
Manan Juneja <manan.juneja@jungleworks.com>
2025-02-12 20:02:37 -0500

    Views and Modifiers projecect init

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Manan Juneja on 2025-02-12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.red)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Manan Juneja on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
