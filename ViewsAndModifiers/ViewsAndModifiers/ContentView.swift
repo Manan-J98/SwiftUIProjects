505c8ec8e8a84371b0228b571720471f357e6142
Manan Juneja <manan.juneja@jungleworks.com>
2025-03-11 19:32:46 -0400

    environment modifiers  and views as properties

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,26 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    @State private var useRedText = false
+    let motto1 = Text("Hello Computer")
+    let motto2 = Text("Hello Human")
+    
+    @ViewBuilder var spells: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    } // swiftui won't apply viewbuilder automatically
+    
+    var spells1: some View {
         VStack {
-            Text("Hello, world!")
-                .padding()
-                .background(.red)
-                .padding()
-                .background(.blue)
-                .padding()
-                .background(.green)
-                .padding()
-                .background(.yellow)
+            Text("Lumos")
+            Text("Obliviate")
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.red)
+    }
         
-//        Button("Hello World") {
-//            
+    
+    var spells2: some View {
+        Group {
+            Text("Lumos")
+            Text("Obliviate")
+        }
+    }
+    var body: some View {
+//        VStack {
+//            Text("Hello, world!")
+//                .padding()
+//                .background(.red)
+//                .padding()
+//                .background(.blue)
+//                .padding()
+//                .background(.green)
+//                .padding()
+//                .background(.yellow)
 //        }
-//        .frame(width: 200, height: 200)
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
 //        .background(.red)
+        
+        VStack {
+            motto1
+                .font(.largeTitle)// takes priority over environment vars
+            Text("World")
+            Text("This is MJ")
+        }
+        .font(.title) // environment var
+        
+        Button("Hello World") {
+            useRedText.toggle()
+        }
+        .frame(width: 200, height: 200)
+        .background(useRedText ? .red : .black) // ternary ops
         
     }
 }
