acdffc3f9e268c5ef478a136e4a0499565d52930
Manan Juneja <manan.juneja@jungleworks.com>
2025-03-20 19:37:58 -0400

    View composition(modularisation) and custom modifiers (conformation to ViewModifier protocol)

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,6 +7,45 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+//            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.green)
+            .clipShape(.rect(cornerRadius: 10.0))
+    }
+}
+
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var useRedText = false
     let motto1 = Text("Hello Computer")
@@ -50,7 +89,13 @@ struct ContentView: View {
             motto1
                 .font(.largeTitle)// takes priority over environment vars
             Text("World")
+                .titleStyle()
+                .watermarked(with: "Manan")
             Text("This is MJ")
+            CapsuleText(text: "This is capsule text")
+                .foregroundStyle(.white)
+            CapsuleText(text: "This is another one")
+                .foregroundStyle(.yellow)
         }
         .font(.title) // environment var
         
@@ -60,6 +105,16 @@ struct ContentView: View {
         .frame(width: 200, height: 200)
         .background(useRedText ? .red : .black) // ternary ops
         
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+    
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
     }
 }
 
